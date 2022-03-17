c86d3b6bc6e84e22c73ff62457f77a33a2c00ac0
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-03-17 08:56:01 -0300

    Inicialização do projeto com Spring. Criação do Modelo de Cliente.

---
+++ src/main/java/io/github/rodrigopaluma/clientes/model/entity/Cliente.java
@@ -0,0 +1,21 @@
+package io.github.rodrigopaluma.clientes.model.entity;
+
+import javax.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 150)
+    private String nome;
+
+    @Column(nullable = false, length = 11)
+    private String cpf;
+
+    @Column
+    private LocalDate dataCadastro;
+}
