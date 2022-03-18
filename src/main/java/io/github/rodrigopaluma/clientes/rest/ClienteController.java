05ba67a638e166f03482200e7727c78a4b7f080a
Rodrigo Paluma <rodrigo.paluma@gmail.com>
2022-03-18 07:41:06 -0300

    Criação do método para apagar cliente.

--- src/main/java/io/github/rodrigopaluma/clientes/rest/ClienteController.java
+++ src/main/java/io/github/rodrigopaluma/clientes/rest/ClienteController.java
@@ -25,9 +25,34 @@ public class ClienteController {
     }
 
     @GetMapping("{id}")
-    public Cliente getId(@PathVariable Integer id){
+    public Cliente getClient(@PathVariable Integer id){
         return repository
                 .findById(id)
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteClient(@PathVariable Integer id){
+        repository
+                .findById(id)
+                .map(cliente -> {
+                    repository.delete(cliente);
+                    return Void.TYPE;
+                })
+                .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateClient(@PathVariable Integer id, @RequestBody Cliente clientUpdate){
+        repository
+                .findById(id)
+                .map(cliente -> {
+                    cliente.setNome(clientUpdate.getNome());
+                    cliente.setCpf(clientUpdate.getCpf());
+                    return repository.save(clientUpdate);
+                })
                 .orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
     }
 
